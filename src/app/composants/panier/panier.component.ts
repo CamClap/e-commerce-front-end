2af1ee174ffc7097ac8c53a7dfe853e0bb91554d
camille <c.clappaz@hotmail.fr>
2021-06-09 16:31:46 +0200

    "html panier"

--- src/app/composants/panier/panier.component.ts
+++ src/app/composants/panier/panier.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LivreService } from 'src/app/services/livre.service';
+// import { livre } from 'src/app/composants/livre';
 
 @Component({
   selector: 'app-panier',
@@ -6,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.css']
 })
 export class PanierComponent implements OnInit {
+  livres ={}
+ 
 
-  constructor() { }
+  constructor(private l: LivreService, private router: Router) { }
 
   ngOnInit(): void {
   }
