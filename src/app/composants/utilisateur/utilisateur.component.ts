24dc3742227da9239537ebfb94875a5887bd9d09
camille <c.clappaz@hotmail.fr>
2021-06-11 14:40:39 +0200

    "icone bonhomme et html utilisateur"

--- src/app/composants/utilisateur/utilisateur.component.ts
+++ src/app/composants/utilisateur/utilisateur.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
+import { Adresse } from 'src/app/interfaces/Adresse';
 
 @Component({
   selector: 'app-utilisateur',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utilisateur.component.css']
 })
 export class UtilisateurComponent implements OnInit {
-
-  constructor() { }
+  user: User = {}
+  formUser = this.fb.group({
+    nom:[''],
+    prenom:[''],
+    email:[''],
+    mdp:[''],
+    adresse: this.fb.group({
+      num:[],
+      rue:[''],
+      cp:[''],
+      ville:[''],
+      complement:['']
+    }),
+    adresseLivraison: this.fb.group({
+      num:[],
+      rue:[''],
+      cp:[''],
+      ville:[''],
+      complement:['']
+    })
+  })
+  constructor(private fb: FormBuilder, private US: UtilisateurService) { }
 
   ngOnInit(): void {
   }
+editUser(){
 
+}
 }
