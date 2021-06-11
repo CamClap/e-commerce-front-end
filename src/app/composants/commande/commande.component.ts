dbff1cf81c622687b361a3885d835e6a4da7c9e8
camille <c.clappaz@hotmail.fr>
2021-06-11 13:13:27 +0200

    "html et un peu ts de commande"

--- src/app/composants/commande/commande.component.ts
+++ src/app/composants/commande/commande.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Commande } from 'src/app/interfaces/Commande';
+import { User } from 'src/app/interfaces/User';
+import { Livre } from 'src/app/interfaces/livre';
+import { CommandeService } from 'src/app/services/commande.service';
 
 @Component({
   selector: 'app-commande',
   templateUrl: './commande.component.html',
-  styleUrls: ['./commande.component.css']
+  styleUrls: ['./commande.component.css'],
 })
 export class CommandeComponent implements OnInit {
+  commande: Commande = {};
+  user: User = {};
+  livre: Livre = {};
+  livres: Livre[] = [];
 
-  constructor() { }
+  constructor(private commandeService: CommandeService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validerCommande() {
+    this.commandeService.addCommande(this.commande).subscribe((res) => {
+      this.commande = {};
+    });
   }
-  validerCommande(){}
+
+  quantiteVoulue() {}
+
+  prixTotal() {}
 }
