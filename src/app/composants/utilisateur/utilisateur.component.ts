54de16198b207901d581d1fe79e4430ce58764c4
camille <c.clappaz@hotmail.fr>
2021-06-12 16:58:18 +0200

    "modif gestion client"

--- src/app/composants/utilisateur/utilisateur.component.ts
+++ src/app/composants/utilisateur/utilisateur.component.ts
@@ -3,43 +3,55 @@ import { User } from 'src/app/interfaces/User';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UtilisateurService } from 'src/app/services/utilisateur.service';
 import { Adresse } from 'src/app/interfaces/Adresse';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthentificationService } from 'src/app/services/authentification.service';
 
 @Component({
   selector: 'app-utilisateur',
   templateUrl: './utilisateur.component.html',
-  styleUrls: ['./utilisateur.component.css']
+  styleUrls: ['./utilisateur.component.css'],
 })
 export class UtilisateurComponent implements OnInit {
-  user: User = {}
+  user: User = {};
   formUser = this.fb.group({
-    nom:[''],
-    prenom:[''],
-    email:[''],
-    mdp:[''],
+    nom: [''],
+    prenom: [''],
+    email: [''],
+    mdp: [''],
     adresse: this.fb.group({
-      num:[],
-      rue:[''],
-      cp:[''],
-      ville:[''],
-      complement:['']
+      num: [],
+      rue: [''],
+      cp: [''],
+      ville: [''],
+      complement: [''],
     }),
     adresseLivraison: this.fb.group({
-      num:[],
-      rue:[''],
-      cp:[''],
-      ville:[''],
-      complement:['']
-    })
-  })
-  router: any;
-  constructor(private fb: FormBuilder, private US: UtilisateurService) { }
+      num: [],
+      rue: [''],
+      cp: [''],
+      ville: [''],
+      complement: [''],
+    }),
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private US: UtilisateurService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private auth: AuthentificationService
+  ) {}
 
   ngOnInit(): void {
+    const connectedUSer = JSON.parse(localStorage.getItem('user'));
+    this.formUser.patchValue(connectedUSer);
+    // this.auth.checkData(this.user).subscribe((res) => {
+    //   this.user = JSON.parse(localStorage.getItem('user'));
+    // });
   }
   modifierUser() {
-    this.US.editUser(this.user).subscribe(res=> {
-      this.router.navigateByUrl('/home');
-    });
-    
+    // this.US.editUser(this.user).subscribe((res) => {
+    //   this.router.navigateByUrl('/home');
+    // });
   }
 }
