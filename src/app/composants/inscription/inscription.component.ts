0400dff03b0da65b48346fd7ecea4d8a72b24733
rmns06 <rmns06@gmail.com>
2021-06-14 11:20:02 +0200

    Modif home

--- src/app/composants/inscription/inscription.component.ts
+++ src/app/composants/inscription/inscription.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/User';
+import { AuthentificationService } from 'src/app/services/authentification.service';
 import { UtilisateurService } from '../../services/utilisateur.service'
 
 @Component({
@@ -9,37 +11,39 @@ import { UtilisateurService } from '../../services/utilisateur.service'
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+  
   user: User = {}
   formUser = this.fb.group({
-    nom:[''],
-    prenom:[''],
-    email:[''],
-    mdp:[''],
+    nom:['Frontino '],
+    prenom:['Allard'],
+    email:['test3@test.com'],
+    mdp:['1234'],
     adresse: this.fb.group({
-      num:[],
-      rue:[''],
-      cp:[''],
-      ville:[''],
+      num:[14],
+      rue:['rue Marie de Médecis'],
+      cp:['64200'],
+      ville:['Biarritz'],
       complement:['']
     }),
     adresseLivraison: this.fb.group({
-      num:[],
-      rue:[''],
-      cp:[''],
-      ville:[''],
+      num:[28],
+      rue:['Avenue de la République'],
+      cp:['84000'],
+      ville:['Avignon'],
       complement:['']
     })
   })
 
 
-  constructor(private utilisateurService: UtilisateurService, private fb: FormBuilder) { }
+  constructor(private utilisateurService: UtilisateurService, private fb: FormBuilder, private router: Router, private auth: AuthentificationService) { }
 
   ngOnInit(): void {
   }
   
   addUser(){
     console.log(this.formUser.value);
-    this.utilisateurService.addPersonne(this.formUser.value).subscribe()
+    this.utilisateurService.addPersonne(this.formUser.value).subscribe();
+    this.router.navigateByUrl('/home');
   }
-
+  
 }
