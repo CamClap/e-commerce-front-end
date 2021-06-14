c7ffd6b9dc4062bbec8b0912a6f38d6efcb5924b
rmns06 <rmns06@gmail.com>
2021-06-14 10:59:10 +0200

    modif css home

--- src/app/composants/commande/commande.component.ts
+++ src/app/composants/commande/commande.component.ts
@@ -3,6 +3,7 @@ import { Commande } from 'src/app/interfaces/Commande';
 import { User } from 'src/app/interfaces/User';
 import { Livre } from 'src/app/interfaces/livre';
 import { CommandeService } from 'src/app/services/commande.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-commande',
@@ -15,13 +16,14 @@ export class CommandeComponent implements OnInit {
   livre: Livre = {};
   livres: Livre[] = [];
 
-  constructor(private commandeService: CommandeService) {}
+  constructor(private commandeService: CommandeService, private router: Router) {}
 
   ngOnInit(): void {}
 
   validerCommande() {
     this.commandeService.addCommande(this.commande).subscribe((res) => {
       this.commande = {};
+      this.router.navigateByUrl('/home');
     });
   }
 
